fbb73b907628cb1f9a8eefe060e526ad6be9ef0b
RikiTikiCzaryMary <speedwaymaniak@gmail.com>
2019-07-28 10:00:04 +0200

    New tests

--- src/test/java/projekt_z_pieniedzmi/PieniadzeTest.java
+++ src/test/java/projekt_z_pieniedzmi/PieniadzeTest.java
@@ -2,6 +2,8 @@ package projekt_z_pieniedzmi;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 
@@ -14,25 +16,35 @@ class PieniadzeTest {
         Pieniadze hajsBiedaka = new Pieniadze(BigDecimal.valueOf(20), Waluta.PLN);
         Pieniadze hajsBogacza = new Pieniadze(BigDecimal.valueOf(80), Waluta.PLN);
         Assertions.assertTrue(hajsBiedaka.czyCieStac(hajsBiedaka));
-        }
+    }
 
-        @Test
+    @Test
     void czyCieNieStac() {
         Pieniadze hajsBiedaka = new Pieniadze(BigDecimal.valueOf(20), Waluta.PLN);
         Pieniadze hajsBogacza = new Pieniadze(BigDecimal.valueOf(80), Waluta.PLN);
         Assertions.assertTrue(hajsBiedaka.czyCieStac(hajsBogacza));
     }
 
-        @Test
+    @Test
     void CzyCieStacGdyMamyTyleSamo() {
         Pieniadze hajsBiedaka = new Pieniadze(BigDecimal.valueOf(20), Waluta.PLN);
         Pieniadze hajsBogacza = new Pieniadze(BigDecimal.valueOf(20), Waluta.PLN);
         Assertions.assertTrue(hajsBogacza.czyCieStac(hajsBiedaka));
     }
-        @Test
-    void CzyWalutaJestTaSama(){
+
+    @Test
+    void CzyWalutaJestTaSama() {
         Pieniadze hajsBiedaka = new Pieniadze(BigDecimal.valueOf(20), Waluta.PLN);
         Pieniadze hajsBogacza = new Pieniadze(BigDecimal.valueOf(20), Waluta.PLN);
-        Assertions.assertEquals(hajsBiedaka.getWaluta(),hajsBogacza.getWaluta());
-        }
+        Assertions.assertEquals(hajsBiedaka.getWaluta(), hajsBogacza.getWaluta());
     }
+
+    @ParameterizedTest
+    @CsvSource({"7, PLN, true", "10.00000, PLN, true", "25, PLN, false"})
+    public void czyCieStacInaczej(String kwota, Waluta waluta, boolean result) {
+        Pieniadze a = new Pieniadze(BigDecimal.TEN, Waluta.PLN);
+        Pieniadze b = new Pieniadze(new BigDecimal(kwota), waluta);
+
+        assertEquals(result, a.czyCieStac(b));
+    }
+}
